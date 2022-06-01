49ec8abb2bf19e70982572e34925293b41acf3d8
yesim <sityesim@gmail.com>
2022-06-01 11:02:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+
+function fetchBooks(){
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((response)=>response.json())
+  .then((json)=>renderBooks(json));
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+    const h1 = document.createElement('h1');
+    h1.innerHTML = book.name;
+    main.appendChild(h1);
   });
 }
 
